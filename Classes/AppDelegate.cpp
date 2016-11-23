8a1cda282c8291237c34499ed4b12d1b04092b44
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-11-24 02:28:48 +0900

    project名とか諸々変えて作りなおしました

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -39,9 +39,9 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto glview = director->getOpenGLView();
     if(!glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glview = GLViewImpl::createWithRect("MyCppGame", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+        glview = GLViewImpl::createWithRect("LifeGarden", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
 #else
-        glview = GLViewImpl::create("MyCppGame");
+        glview = GLViewImpl::create("LifeGarden");
 #endif
         director->setOpenGLView(glview);
     }
